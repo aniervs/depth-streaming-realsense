677d1eecfe97d5289a1b18dcc57a0d1c00e5b53d
Anier Velasco Sotomayor <anier.velasco@gmail.com>
2023-06-21 16:22:37 +0200

    Add script to capture and export to MP4 the rgb and depth images of Realsense camera.

---
+++ main.py
@@ -0,0 +1,61 @@
+import pyrealsense2 as rs
+import cv2
+import numpy as np
+
+filename = input('Enter output file name: ')
+rgb_video = filename + ".mp4"
+depth_video = filename + "_depth.mp4"
+
+pipeline = rs.pipeline()
+config = rs.config()
+config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+
+pipeline.start(config)
+
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+fps = 30
+video_writer_rgb = None
+video_writer_depth = None
+
+try:
+    while True:
+        frames = pipeline.wait_for_frames()
+        color_frame = frames.get_color_frame()
+        depth_frame = frames.get_depth_frame()
+
+        if not color_frame or not depth_frame:
+            continue
+
+        color_image = np.asanyarray(color_frame.get_data())
+        depth_image = np.asanyarray(depth_frame.get_data())
+
+        if video_writer_rgb is None or video_writer_depth is None:
+            height_rgb, width_rgb = color_image.shape[:2]
+            height_depth, width_depth = depth_image.shape[:2]
+
+            video_writer_rgb = cv2.VideoWriter(rgb_video, fourcc, fps, (width_rgb, height_rgb))
+            video_writer_depth = cv2.VideoWriter(depth_video, fourcc, fps, (width_depth, height_depth))
+
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.25), cv2.COLORMAP_RAINBOW)
+
+        video_writer_depth.write(depth_colormap)
+        video_writer_rgb.write(color_image)
+
+        cv2.imshow('RealSense Capture', color_image)
+        cv2.imshow('RealSense Depth', depth_colormap)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+finally:
+    # Stop the RealSense pipeline
+    pipeline.stop()
+
+    # Release the video writer
+    if video_writer_rgb is not None and video_writer_depth:
+        video_writer_rgb.release()
+        video_writer_depth.release()
+
+    # Close all OpenCV windows
+    cv2.destroyAllWindows()
+
